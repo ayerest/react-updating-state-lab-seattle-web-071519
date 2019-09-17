25a33640d5902833731cc9b429305f01fe36a548
ayerest <irisiseve@gmail.com>
2019-09-17 09:57:01 -0700

    finished react updating state lab

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,37 @@
 // Code DigitalClicker Component Here
+
+import React from 'react'
+
+class DigitalClicker extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            timesClicked: 0
+        }   
+    }
+
+    // incrementClicker = async () => {
+    //     await this.setState({
+    //         timesClicked: parseInt(this.state.timesClicked + 1)
+    //     })
+        
+    // }
+
+    incrementClicker =  () => {
+         this.setState({
+            timesClicked: parseInt(this.state.timesClicked + 1)
+        })
+
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.incrementClicker}>{this.state.timesClicked}</button>
+            </div>
+        )
+    }
+
+}
+
+export default DigitalClicker;
